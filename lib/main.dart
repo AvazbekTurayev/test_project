f1aa962dd20ad49b99794b2cdeab837f5a0f4ff3
AvazbekTurayev <avazbekturayev2311@gmail.com>
2022-01-07 00:39:32 +0500

    Change Online to Tutorial

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:test_project/pages/detail_page.dart';
+import 'package:test_project/pages/home_page.dart';
+
+
+
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+      routes: {
+        HomePage.id: (context) => HomePage(),
+        DetailPage.id: (context) => DetailPage()
+      },
+    );
+  }
+}
+
